645cc617ca594ab2a620aaca5c9869ea598eabbb
Kenza Roulleaux-Siouahe <kenza.roulleaux.siouahe@ehb.be>
2022-10-10 15:23:36 +0200

    redid the correct picture

--- spaceInvader.js
+++ spaceInvader.js
@@ -12,105 +12,68 @@ function colorBox() {
 //big black square
     context.beginPath();
     context.rect(10,10,290,290)
+    context.fillStyle="rgba(240,240,240,255)";
     context.fill();
+    context.strokeStyle="rgba(240,240,240,255)";
     context.stroke();
 
-//Ll
-context.beginPath();
-    context.rect(30,90,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
-    context.fill();
-    context.stroke();
-
-//Lm
+//Lmidden
 context.beginPath();
     context.rect(80,90,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
   
-//M   
+//Mmidden  
 context.beginPath();
     context.rect(130,90,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
 
-//Rm
+//Rmidden
 context.beginPath();
     context.rect(180,90,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
-    context.fill();
-    context.stroke();
-
-//Rr    
-context.beginPath();
-    context.rect(230,90,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
  
-//Lmboven 
+//Mboven 
 context.beginPath();
-    context.rect(80,40,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.rect(130,40,50,50)
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
 
-//Rmboven
+//Lbeneden
 context.beginPath();
-    context.rect(180,40,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.rect(80,140,50,50)
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
 
-//Mbeneden
+//Rbeneden
 context.beginPath();
-    context.rect(130,140,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.rect(180,140,50,50)
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
 
+//Mbbeneden
 context.beginPath();
-    context.rect(30,190,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
+    context.rect(130,190,50,50)
+    context.fillStyle="rgba(148,226,155,255)";
+    context.strokeStyle="rgba(148,226,155,255)";
     context.fill();
     context.stroke();
 
-context.beginPath();
-    context.rect(30,240,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
-    context.fill();
-    context.stroke();
 
-context.beginPath();
-    context.rect(130,240,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
-    context.fill();
-    context.stroke();
 
-context.beginPath();
-    context.rect(230,190,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
-    context.fill();
-    context.stroke();
 
-context.beginPath();
-    context.rect(230,240,50,50)
-    context.fillStyle="rgba(0,255,0,255)";
-    context.strokeStyle="rgba(0,255,0,255)";
-    context.fill();
-    context.stroke();
 }
